e1ece72a39620c7afab3e927e3af7968dbe4057d
Chad Miller <chadtmiller15@gmail.com>
2014-01-16 11:51:51 -0800

    fixed formatting

--- js/app.js
+++ js/app.js
@@ -2,50 +2,50 @@
 
 (function () {
 
-	var getRSS = function(link) {
+  var getRSS = function(link) {
 
-		var promise = new Ember.RSVP.Promise(function(resolve, reject){
-		  google.load("feeds", "1", {"callback" : handler});
+    var promise = new Ember.RSVP.Promise(function(resolve, reject){
+      google.load("feeds", "1", {"callback" : handler});
 
-		  function handler() {
-				var feed = new google.feeds.Feed(link);
-				feed.includeHistoricalEntries();
-				feed.setNumEntries(30);
+      function handler() {
+        var feed = new google.feeds.Feed(link);
+        feed.includeHistoricalEntries();
+        feed.setNumEntries(30);
 
-				feed.load(function(result) {
-			    if (!result.error) {
-			      var links = [];
+        feed.load(function(result) {
+          if (!result.error) {
+            var links = [];
 
-			      result.feed.entries.forEach(function (entry) {
-			      	links.push(entry);
-			      });
+            result.feed.entries.forEach(function (entry) {
+              links.push(entry);
+            });
 
-			      resolve(links);
-			    }
-			  });
-			}
-		});
+            resolve(links);
+          }
+        });
+      }
+    });
 
-		return promise;
-	}
+    return promise;
+  }
 
-	EmberVerge = Ember.Application.create();
+  EmberVerge = Ember.Application.create();
 
-	EmberVerge.Hubs = Ember.Object.extend();
+  EmberVerge.Hubs = Ember.Object.extend();
 
-	EmberVerge.Hubs.reopenClass({
+  EmberVerge.Hubs.reopenClass({
 
-		findAll: function(hub){
-			return getRSS("http://www.theverge.com/rss/index.xml");
-		},
+    findAll: function(hub){
+      return getRSS("http://www.theverge.com/rss/index.xml");
+    },
 
-		findByHub: function(hub){
-			return getRSS("http://www.theverge.com/" + hub + "/rss/index.xml");
-		}
+    findByHub: function(hub){
+      return getRSS("http://www.theverge.com/" + hub + "/rss/index.xml");
+    }
 
-	})
+  })
 
-	// Routes below
+  // Routes below
   EmberVerge.Router.map(function() {
     this.resource("links", { path: "/" });
   });
@@ -56,6 +56,6 @@
     }
   });
 
-	EmberVerge.LoadingRoute = Ember.Route.extend({});
+  EmberVerge.LoadingRoute = Ember.Route.extend({});
 
 })();
